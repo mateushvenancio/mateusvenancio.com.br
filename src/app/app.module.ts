7e0a1eafe43c8bce770f2ad083983caac2aab9b4
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2021-01-28 15:23:05 -0300

    atualizacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeHeaderComponent } from './components/home/home-header/home-header.component';
+import { HomeAboutComponent } from './components/home/home-about/home-about.component';
+import { HomeProjectsComponent } from './components/home/home-projects/home-projects.component';
+import { HomeBlogComponent } from './components/home/home-blog/home-blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomeHeaderComponent
+    HomeHeaderComponent,
+    HomeAboutComponent,
+    HomeProjectsComponent,
+    HomeBlogComponent
   ],
   imports: [
     BrowserModule,
