bfae444b277730d73288c39852e9b8e35068f644
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2021-01-29 16:34:55 -0300

    header atualizado

--- src/app/components/home/home-header/home-header.component.ts
+++ src/app/components/home/home-header/home-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faInstagram, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { ProfileModel } from 'src/app/models/profile.model';
 
 @Component({
     selector: 'app-home-header',
@@ -11,10 +12,20 @@ export class HomeHeaderComponent implements OnInit {
     iconeGithub = faGithub
     iconeLinkedin = faLinkedin
     iconeMedium = faMedium
+    iconeInstagram = faInstagram
+
+    profileInfo: ProfileModel
+
+    profileInfos: ProfileModel[] = [
+        new ProfileModel('https://www.jbox.com.br/wp/wp-content/uploads/2020/08/pok%C3%A9mon-koko-filme-destacada.jpg', 'I see now that one’s birth is irrelevant. It’s what you do that determines who you are.'),
+        new ProfileModel('https://2.bp.blogspot.com/-qS7XRe8KRuA/UpDMxKYvJeI/AAAAAAAAHzo/6rzdifeYonQ/s1600/WhoCookiesSugarBeanBakersBB.jpg', 'The universe is big. It’s vast and complicated and ridiculous. And sometimes, very rarely, impossible things just happen and we call them miracles.'),
+        new ProfileModel('https://64.media.tumblr.com/8bdd29006355ffd036d3df2e61a2184d/tumblr_inline_o8vkcfotu51s3mkdf_250.png', 'The right thing... what is it? I wonder, if you do the right thing, does it really make everyone happy?'),
+    ]
 
     constructor() { }
 
     ngOnInit(): void {
+        this.profileInfo = this.profileInfos[Math.floor(Math.random() * this.profileInfos.length)]
     }
 
 }
